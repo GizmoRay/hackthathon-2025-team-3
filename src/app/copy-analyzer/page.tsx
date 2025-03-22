e223725daf37ce1dd2e8c259d79e5a5c7fdb545d
Benjamin Rotholtz <elijah@rotholtz.com>
2025-03-21 18:04:05 -0700

    Make it mobile friendly

--- src/app/copy-analyzer/page.tsx
+++ src/app/copy-analyzer/page.tsx
@@ -61,7 +61,7 @@ export default function CopyAnalyzer() {
 	};
 
 	return (
-		<div className={styles.analyzerContainer}>
+		<div className={styles.container}>
 			<Sidebar type="stats">
 				<div className={styles.readabilityStats}>
 					<h2>Readability rating:</h2>
@@ -103,64 +103,67 @@ export default function CopyAnalyzer() {
 				)}
 			</Sidebar>
 
-			<div className={styles.mainContent}>
+			<div className={styles.mainColumn}>
 				<Title
 					title="Intelligent Style Guide Toolkit"
 					description="Input your text in the box below. Errors in grammar, spelling, and brand voice will appear with highlights. Click the suggestions to learn more."
 				/>
-				<form onSubmit={handleSubmit}>
-					<div className={styles.textareaWrapper}>
-						<textarea
-							value={text}
-							onChange={(e) => setText(e.target.value)}
-							placeholder="Paste your copy here..."
-							className={styles.textInput}
-							onKeyDown={(e) => {
-								if (e.key === "Enter" && !e.shiftKey) {
-									e.preventDefault();
-									handleSubmit(e);
-								}
-							}}
-						/>
-						<div
-							className={`${styles.spinner} ${
-								isAnalyzing ? styles.visible : ""
-							}`}
-						/>
-					</div>
-				</form>
+
+				<div className={styles.editorContainer}>
+					<form onSubmit={handleSubmit}>
+						<div className={styles.textareaWrapper}>
+							<textarea
+								value={text}
+								onChange={(e) => setText(e.target.value)}
+								placeholder="Paste your copy here..."
+								className={styles.textInput}
+								onKeyDown={(e) => {
+									if (e.key === "Enter" && !e.shiftKey) {
+										e.preventDefault();
+										handleSubmit(e);
+									}
+								}}
+							/>
+							<div
+								className={`${styles.spinner} ${
+									isAnalyzing ? styles.visible : ""
+								}`}
+							/>
+						</div>
+					</form>
+
+					{feedback && (
+						<Sidebar type="list" className={styles.recommendationsColumn}>
+							<div className={`${styles.grammarSection} ${styles.statItem}`}>
+								<h3>Grammar suggestion:</h3>
+								<ul>
+									{feedback?.grammar.issues.map((issue, index) => (
+										<li key={`grammar-${index}`}>{issue}</li>
+									))}
+								</ul>
+							</div>
+
+							<div className={`${styles.legalSection} ${styles.statItem}`}>
+								<h3>Legal suggestion:</h3>
+								<ul>
+									{feedback?.legal.issues.map((issue, index) => (
+										<li key={`legal-${index}`}>{issue}</li>
+									))}
+								</ul>
+							</div>
+
+							<div className={`${styles.brandSection} ${styles.statItem}`}>
+								<h3>Brand Voice and Tone:</h3>
+								<ul>
+									{feedback?.brandVoice.issues.map((issue, index) => (
+										<li key={`brand-${index}`}>{issue}</li>
+									))}
+								</ul>
+							</div>
+						</Sidebar>
+					)}
+				</div>
 			</div>
-
-			{feedback && (
-				<Sidebar type="list" className={styles.recommendationsColumn}>
-					<div className={`${styles.grammarSection} ${styles.statItem}`}>
-						<h3>Grammar suggestion:</h3>
-						<ul>
-							{feedback?.grammar.issues.map((issue, index) => (
-								<li key={`grammar-${index}`}>{issue}</li>
-							))}
-						</ul>
-					</div>
-
-					<div className={`${styles.legalSection} ${styles.statItem}`}>
-						<h3>Legal suggestion:</h3>
-						<ul>
-							{feedback?.legal.issues.map((issue, index) => (
-								<li key={`legal-${index}`}>{issue}</li>
-							))}
-						</ul>
-					</div>
-
-					<div className={`${styles.brandSection} ${styles.statItem}`}>
-						<h3>Brand Voice and Tone:</h3>
-						<ul>
-							{feedback?.brandVoice.issues.map((issue, index) => (
-								<li key={`brand-${index}`}>{issue}</li>
-							))}
-						</ul>
-					</div>
-				</Sidebar>
-			)}
 		</div>
 	);
 }
