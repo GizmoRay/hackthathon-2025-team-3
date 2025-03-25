b3159bb8f0b5da5bd9a189f347bdba7ea1ad398d
GizmoRay <elijah@rotholtz.com>
2025-03-25 14:19:44 -0700

    Logo link

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -12,15 +12,17 @@ const Navigation: FC = () => {
 	return (
 		<nav className={styles.nav}>
 			<div className={styles.container}>
-				<div className={styles.logo}>
-					<Image
-						src="/Stars.svg"
-						alt="ServiceNow Intelligent Style Guide"
-						width={21}
-						height={21}
-					/>
-				</div>
-				<div className={styles.title}>ServiceNow Intelligent Style Guide</div>
+				<Link href="/" className={styles.logoContainer}>
+					<div className={styles.logo}>
+						<Image
+							src="/Stars.svg"
+							alt="ServiceNow Intelligent Style Guide"
+							width={21}
+							height={21}
+						/>
+					</div>
+					<div className={styles.title}>ServiceNow Intelligent Style Guide</div>
+				</Link>
 				<nav className={styles.navLinks}>
 					<Link
 						href="/copy-analyzer"
