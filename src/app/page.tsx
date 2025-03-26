545be98933a46e05c86af5c849e605aebdde4791
Benjamin Rotholtz <elijah@rotholtz.com>
2025-03-25 21:27:00 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import Title from "@/components/Title/Title";
 export default function Home() {
 	return (
 		<div className={styles.hub}>
-			/*<Image
+			{/*<Image
 				src={"/Banner.png"}
 				width={952}
 				height={238}
 				alt="Banner image for overview page. We are One Voice"
-			/>*/
+			/>*/}
 			<Title
 				type="hub"
 				title="Intelligent Style Guide Toolkit"
